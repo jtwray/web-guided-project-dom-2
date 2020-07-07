5aafe3786d58d02ddb257c6b4af08840b121da2d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-07 19:01:51 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -40,7 +40,9 @@ launchButton.addEventListener('click', () => {
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
-
+launchButton.addEventListener('click', () => {
+  modal.classList.remove('off')
+})
 
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
